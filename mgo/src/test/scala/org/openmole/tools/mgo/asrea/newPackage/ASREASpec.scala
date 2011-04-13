961ac312917f6c56f743f5eec924c02edad79201
Romain Reuillon <romain.reuillon@iscpif.fr>
2011-04-13 19:07:05 +0200

    Add test.

---
+++ mgo/src/test/scala/org/openmole/tools/mgo/asrea/newPackage/ASREASpec.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2011 reuillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.tools.mgo.asrea.newPackage
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+import java.io.File
+
+import scala.collection.mutable.ListBuffer
+
+@RunWith(classOf[JUnitRunner])
+class ASREASpec extends FlatSpec with ShouldMatchers {
+
+  "ASREA" should "converge to good solutions" in {
+    true should equal(true)
+  }
+  
+}
+
