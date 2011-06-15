8cf191c75cd7d4292bd0e0ca7f466f3dba61ec81
Sebastien Rey <reyman64@gmail.com>
2011-06-15 09:58:41 +0200

    modif gaussian mutation

--- mgo/src/main/scala/org/openmole/tools/mgo/mappedgenome/genomedouble/SoftMutation.scala
+++ mgo/src/main/scala/org/openmole/tools/mgo/mappedgenome/genomedouble/SoftMutation.scala
@@ -7,32 +7,20 @@ package org.openmole.tools.mgo.mappedgenome.genomedouble
 import org.openmole.tools.mgo.evolution.GenomeOperation
 import org.openmole.tools.mgo.mappedgenome.genomedouble._
 import org.apache.commons.math.util.FastMath._
-import org.apache.commons.math.random.GaussianRandomGenerator
 import org.openmole.tools.mgo.tools.Random._
 import java.util.Random
 
-class SoftMutation (interval: IntervalSet,sigma:Double, mean:Double,rate: Random => Double = rng => rng.nextFloat) extends GenomeOperation[GenomeDouble] {
+class SoftMutation (interval: IntervalSet,rate: Random => Double = rng => rng.nextFloat) extends GenomeOperation[GenomeDouble] {
 
-  def this(interval:IntervalSet,sigma:Double, mean:Double, rate: Double) = this(interval, sigma, mean, _ => rate)
+  def this(interval:IntervalSet, rate: Double) = this(interval,  _ => rate)
   
   override def operate(genomes: IndexedSeq[GenomeDouble])(implicit rng: Random): GenomeDouble = {
 
-  val size:Int = genomes.size
-  
-  //FIXME : A remplacer par genomes.random a cause du decorateur de Romain
-  val pickedGenome:GenomeDouble = genomes.random
-  
-    val gRnd = rng.nextGaussian()
-    
-    //on fait un ajustement fonction de sigma et mean
-    //http://c-faq.com/lib/gaussian.html
-    //http://www.developpez.net/forums/d331848/autres-langages/algorithmes/contribuez/generation-nombre-aleatoire-suivant-loi-gaussienne/
-    val gRndAffine =  ( gRnd  * sigma ) + mean
-    
-    val mutationRate = rate(rng)
-    
+    val size:Int = genomes.size
+    val pickedGenome:GenomeDouble = genomes.random
     val mapOfBounds = IntervalSet.intervallDecorator.generateBounds(interval)
     val newGenome = IntervalSet.intervallDecorator.generateEmpty(interval)
+    val mutationRate = rate(rng)
     
     pickedGenome.foreach{ case(key,value) => {
         newGenome.update(key, {
@@ -40,7 +28,8 @@ class SoftMutation (interval: IntervalSet,sigma:Double, mean:Double,rate: Random
             pickedGenome.apply(key)
           else
           { 
-            clamp(gRndAffine,mapOfBounds.apply(key)._1,mapOfBounds.apply(key)._2)
+            0.0
+            //gaussianMutation(value,mean,sigma,mapOfBounds.apply(key)._1,mapOfBounds.apply(key)._2)
           }
         }
        )
@@ -49,6 +38,17 @@ class SoftMutation (interval: IntervalSet,sigma:Double, mean:Double,rate: Random
   
   return newGenome
   
+  }
+  
+  def gaussianMutation(value:Double,sigma:Double,mean:Double,min:Double,max:Double)(implicit rng: Random):Double ={
+
+    //on fait un ajustement fonction de sigma et mean
+    //http://c-faq.com/lib/gaussian.html
+    //http://www.developpez.net/forums/d331848/autres-langages/algorithmes/contribuez/generation-nombre-aleatoire-suivant-loi-gaussienne/
+    val gRnd = rng.nextGaussian()
+    val gRndAffine =  ( gRnd  * sigma ) + mean
+    
+    return clamp(gRndAffine,min,max)
   }
   
   def clamp( value:Double, min:Double, max:Double):Double={
