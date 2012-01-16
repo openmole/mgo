98156e5ec2c8f5c2cc3df08203c5981080f97e11
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-01-16 17:08:25 +0100

    Merge.

--- mgo/src/test/scala/org/openmole/tools/mgo/GenomeParametersAckleySpec.scala
+++ mgo/src/test/scala/org/openmole/tools/mgo/GenomeParametersAckleySpec.scala
@@ -32,11 +32,7 @@ class GenomeParametersAckleySpec extends FlatSpec with ShouldMatchers{
       def wrappedValues = values ++ sigma
     }
     
-<<<<<<< HEAD
     class GenomeSLocalFactory extends GAGenomeFactory[GenomeSLocal] with GASigmaParametersFactory [GenomeSLocal] {
-=======
-    implicit object GenomeAckleySigmaFactory extends GAGenomeSigmaFactory [GenomeAckley] {
->>>>>>> b3847c30c8c7540f0e2b71a2b5cee5af241fc307
       override def buildGenome(v : IndexedSeq[Double]) = 
         new GenomeAckley {
           val values = v.slice(0, 2)
@@ -70,20 +66,9 @@ class GenomeParametersAckleySpec extends FlatSpec with ShouldMatchers{
      }*/
     
     // http://tracer.lcc.uma.es/problems/ackley/ackley.html
-<<<<<<< HEAD
     def evaluator(inGenome: GenomeSLocal) = {
       // Nombre de dimensions de la fonction = nombre de gene dans le genome
       val genomeSize:Double = inGenome.values.size
-=======
-    implicit object IndividuFactory 
-    extends IndividualMGFactory[MultiGoal,GenomeAckley,IndividualMG[GenomeAckley,MultiGoal] with IRanking with IDistance]
-    {
-        
-      override def operate(genome:GenomeAckley):IndividualMG[GenomeAckley,MultiGoal] with IRanking with IDistance = {
-    
-        // Nombre de dimensions de la fonction = nombre de gene dans le genome
-        val genomeSize:Double = genome.values.size
->>>>>>> b3847c30c8c7540f0e2b71a2b5cee5af241fc307
         
       //val max:Double = 1 
       //val min:Double = 0
@@ -114,7 +99,6 @@ class GenomeParametersAckleySpec extends FlatSpec with ShouldMatchers{
       implicit val aprng = new Random
       implicit val function: Random => Double = arpng => arpng.nextFloat
     
-<<<<<<< HEAD
       // Init random population
       var genomes: IndexedSeq[GenomeSLocal] = (0 until 100).map{_ => factory.buildRandomGenome}
     
@@ -135,45 +119,6 @@ class GenomeParametersAckleySpec extends FlatSpec with ShouldMatchers{
           val result = evolutionEngine(acc, factory)
           println("generation" + gen)
           result
-=======
-      // Init random population, equal to 200 genomes here
-      var genomes:IndexedSeq[GenomeAckley] = (0 until 200).map{_ => GenomeAckleySigmaFactory.buildRandomGenome}
-    
-      // Init de l'archive population, vide au premier tour
-      var archive = new PopulationMG[GenomeAckley,IndividualMG[GenomeAckley,MultiGoal] with IRanking with IDistance](IndexedSeq.empty)
-    
-      //val randomMut = new RandomWrappedValuesMutation[GenomeSLocal,GAGenomeSigmaFactory[GenomeSLocal]] (rate => 0.1d)(GenomeSLocalSigmaFactory)
-      val softMut = new CoEvolvingSigmaValuesMutation[GenomeAckley,GAGenomeSigmaFactory[GenomeAckley]] 
-      val randomCross = new RandomWrappedValuesCrossOver[GenomeAckley,GAGenomeSigmaFactory[GenomeAckley]] (rate => 0.5d)(GenomeAckleySigmaFactory)
-      val selectOp = new BinaryTournamentNSGA2[GenomeAckley,MultiGoal,GAGenomeSigmaFactory[GenomeAckley]]()
-     
-      // Init algorithms NSGA2 avec les trois types d'operateurs
-      val evolutionEngine = new NSGAII[MultiGoal,GenomeAckley,GAGenomeSigmaFactory[GenomeAckley]](softMut,selectOp,randomCross)
-       
-      // Init archive with individuals, result of evaluation of random genomes previously generated (200 individuals here)
-      var individus:IndexedSeq[IndividualMG[GenomeAckley,MultiGoal] with IRanking with IDistance] = genomes.map{g => IndividuFactory.operate(g)}
-      
-      //Generation evolve
-      for (i <- 0 to 8000){
-        //Evaluation de la fitness et cr??ation des individu a partir des genomes
-        //println("Evaluate fitness to make new population of individuals")
-        //var population = new PopulationMG [GenomeSLocal](genomes.map{e => IndividuFactory.build(e)})
-        
-        // 1- first last arg, here 100 => indicate us the size of sampling of individuals, 
-        // so it's the number of individuals we want to conserve after merging new individuals with archive individuals, 
-        // This number represent a constant size of our best individuals we want to conserve into archive
-        // 2 - second last arg, here 200 => give us the size of offspring generated, based on a matted population, 
-        // result of our selection operator on the new best archive after sampling
-        var result = evolutionEngine.operate(individus,archive,100,200)
-        
-        individus = result._1.map{g => IndividuFactory.operate(g)}
-        archive = new PopulationMG(result._2)
-        //println("new list > " + genomes.map{e=> e.wrappedValues})
-       
-        //archive = bestPop
-     
-        println("generation" + i)
->>>>>>> b3847c30c8c7540f0e2b71a2b5cee5af241fc307
       }
 
       println(archive.map{i => i.fitness.toString})
