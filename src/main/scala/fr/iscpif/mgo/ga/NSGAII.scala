547e8cbd9be520cfa726ccfd2c30742fc5d66917
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-04-19 09:58:49 +0200

    Remove usless trait.

--- src/main/scala/fr/iscpif/mgo/ga/NSGAII.scala
+++ src/main/scala/fr/iscpif/mgo/ga/NSGAII.scala
@@ -22,10 +22,10 @@ import scala.annotation.tailrec
 object NSGAII {
 
   trait NSGAIISigmaEvolution extends NSGAII with SigmaGAEvolution {
-      type G = GAGenomeWithSigma
-      type F = GAGenomeWithSigmaFactory
-      type I = Individual[GAGenomeWithSigma, Fitness] with Diversity with Rank
-      type FIT = Fitness
+    type G = GAGenomeWithSigma
+    type F = GAGenomeWithSigmaFactory
+    type I = Individual[GAGenomeWithSigma, Fitness] with Diversity with Rank
+    type FIT = Fitness
   }
   
   class NSGAIISigma(
@@ -34,32 +34,35 @@ object NSGAII {
     val archiveSize: Int,
     val evaluator: GAGenomeWithSigma => Fitness,
     val genomeSize: Int
-    ) extends NSGAIISigmaEvolution
-      with MGBinaryTournamentSelection
-      with FirstRankedSteadyTermination
-      with NonDominatedSortingElitism
-      with CoEvolvingSigmaValuesMutation
-      with SBXBoundedCrossover 
-      with CrowdingDistance
-      with ParetoRanking
-      with StrictDominance {
-   
-        def factory = new GAGenomeWithSigmaFactory {
-          def size = genomeSize
-        }
+  ) extends NSGAII with SigmaGAEvolution
+       with MGBinaryTournamentSelection
+       with FirstRankedSteadyTermination
+       with NonDominatedSortingElitism
+       with CoEvolvingSigmaValuesMutation
+       with SBXBoundedCrossover 
+       with CrowdingDistance
+       with ParetoRanking
+       with StrictDominance {
+    type G = GAGenomeWithSigma
+    type F = GAGenomeWithSigmaFactory
+    type I = Individual[GAGenomeWithSigma, Fitness] with Diversity with Rank
+    type FIT = Fitness
+    def factory = new GAGenomeWithSigmaFactory {
+      def size = genomeSize
+    }
     
-      }
+  }
   
 
 }
 
 // WORK NOTE (A ne pas effacer) :
 /*
-Selection occurs two times in the evolutionary loop.First, in order to generate offsprings,
-parents must be selected from the current population (mating selection).Second, the new
-parent population has to be selected from the offspring and the previous parents (Environmental selection)
-A number of selection operators were proposed, which usually base the chance of selection of
-particular individuals on their fitness values or their rank in the population, respectively.
+ Selection occurs two times in the evolutionary loop.First, in order to generate offsprings,
+ parents must be selected from the current population (mating selection).Second, the new
+ parent population has to be selected from the offspring and the previous parents (Environmental selection)
+ A number of selection operators were proposed, which usually base the chance of selection of
+ particular individuals on their fitness values or their rank in the population, respectively.
 
 
 
@@ -67,74 +70,74 @@ particular individuals on their fitness values or their rank in the population, 
 // How to prevent non-dominated solutions from being lost?
 // Environmental selection is used to obtain a representative efficient set
 
-*/
+ */
 
 // @fixme Refaire un check sur Ranking
 
-trait NSGAII extends GAEvolution with MG with Archive with Elitism with DiversityMetric {
+ trait NSGAII extends GAEvolution with MG with Archive with Elitism with DiversityMetric {
 
-  type I <: Individual[G, FIT] with Diversity with Rank
+    type I <: Individual[G, FIT] with Diversity with Rank
   
-  def archiveSize: Int
+    def archiveSize: Int
  
-  implicit def indiv2IndivWithRankAndDistance(
-    i: Iterable[Individual[G, Fitness]]
-  ) = buildIndividualsWithDistanceAndRanking(i.toIndexedSeq)
+    implicit def indiv2IndivWithRankAndDistance(
+      i: Iterable[Individual[G, Fitness]]
+    ) = buildIndividualsWithDistanceAndRanking(i.toIndexedSeq)
  
-  def buildIndividualsWithDistanceAndRanking(
-    individuals: IndexedSeq[Individual[G, Fitness]]
-  ): IndexedSeq[I] = {
-    val ranks = rank(individuals)
-    val distances = diversity(individuals)
-    (individuals zip ranks zip distances) map {
-      case ((i, iranking), idistance) =>
-        new Individual[G, Fitness] with Diversity with Rank {
-          val genome = i.genome
-          val fitness = i.fitness
-          val diversity = idistance.diversity
-          val rank = iranking.rank
-        }
-    }
-  }
-  
-
-  override def evolve(population: IndexedSeq[I])(implicit aprng: Random): IndexedSeq[I] = {
-
-    val offspring = breed(
-      population,
-      population.size
-    ).map {
-      g => Individual(g, evaluator)
-    }
-
-    val archive = population ++ offspring
-
-    //Elitisme strategy
-    val individuals = buildIndividualsWithDistanceAndRanking(archive)
-    elitism(individuals)
-  }
-
-  def breed(
-    archive: IndexedSeq[I],
-    offSpringSize: Int
-  )(implicit aprng: Random): IndexedSeq[G] = {
-
-    //Crossover sur matingPopulation puis mutation
-    def breed(acc: List[G] = List.empty): List[G] = {
-      if (acc.size >= offSpringSize) acc
-      else {
-        val newIndividuals = crossover(
-          selection(archive).genome,
-          selection(archive).genome,
-          factory).map {
-          mutate(_, factory)
-        }.take(offSpringSize).toIndexedSeq
-        breed(acc ++ newIndividuals)
+    def buildIndividualsWithDistanceAndRanking(
+      individuals: IndexedSeq[Individual[G, Fitness]]
+    ): IndexedSeq[I] = {
+      val ranks = rank(individuals)
+      val distances = diversity(individuals)
+      (individuals zip ranks zip distances) map {
+        case ((i, iranking), idistance) =>
+          new Individual[G, Fitness] with Diversity with Rank {
+            val genome = i.genome
+            val fitness = i.fitness
+            val diversity = idistance.diversity
+            val rank = iranking.rank
+          }
       }
     }
+  
 
-    breed().toIndexedSeq
+    override def evolve(population: IndexedSeq[I])(implicit aprng: Random): IndexedSeq[I] = {
+
+      val offspring = breed(
+        population,
+        population.size
+      ).map {
+        g => Individual(g, evaluator)
+      }
+
+      val archive = population ++ offspring
+
+      //Elitisme strategy
+      val individuals = buildIndividualsWithDistanceAndRanking(archive)
+      elitism(individuals)
+    }
+
+    def breed(
+      archive: IndexedSeq[I],
+      offSpringSize: Int
+    )(implicit aprng: Random): IndexedSeq[G] = {
+
+      //Crossover sur matingPopulation puis mutation
+      def breed(acc: List[G] = List.empty): List[G] = {
+        if (acc.size >= offSpringSize) acc
+        else {
+          val newIndividuals = crossover(
+            selection(archive).genome,
+            selection(archive).genome,
+            factory).map {
+            mutate(_, factory)
+          }.take(offSpringSize).toIndexedSeq
+          breed(acc ++ newIndividuals)
+        }
+      }
+
+      breed().toIndexedSeq
+    }
+
+
   }
-
-
-}
