6055813a94f18469f17380e3c688361015bc84b6
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-07-06 20:14:43 +0200

    Use case class for population element.

--- src/main/scala/fr/iscpif/mgo/Population.scala
+++ src/main/scala/fr/iscpif/mgo/Population.scala
@@ -12,41 +12,19 @@ object Population {
 }
 
 object PopulationElement {
-  def apply[G, MF](genome: G, fitness: Fitness, metaFitness: MF) = {
-    val (g, f, mf) = (genome, fitness, metaFitness)
-    new PopulationElement[G, MF] {
-      def genome = g
-      def fitness = f
-      def metaFitness = mf
-    }
-  }
-  
   def apply[G, MF](i: Individual[G], mf: MF) = 
-    new PopulationElement[G, MF] {
-      lazy val genome = i.genome
-      lazy val fitness = i.fitness
-      lazy val metaFitness = mf
-    }
+    new PopulationElement[G, MF](i.genome, i.fitness, mf)
 }
 
 trait Population[+G, +MF]  {
-   
+
   def content: IndexedSeq[PopulationElement[G, MF]]
-  
   def individuals: IndexedSeq[Individual[G]] = content map { _.toIndividual }
   
-  override def toString = content.toString
-  
+  override def toString = content.toString  
 }
 
-trait PopulationElement[+G, +MF] {
-  def genome: G
-  def fitness: Fitness
-  def metaFitness: MF
-  
+case class PopulationElement[+G, +MF](val genome: G, val fitness: Fitness, val metaFitness: MF) {
   def toIndividual = Individual(genome, fitness)
-  
-  //override def equals(o: Any) = (genome, fitness, individual).equals(o)
-  //override def hashCode = (genome, fitness, individual).hashCode
   override def toString = "(genome = " + genome + ", fitness = " + fitness + ", metaFitness = " + metaFitness + ")"
 }
