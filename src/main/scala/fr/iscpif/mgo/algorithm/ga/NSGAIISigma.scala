342bd1ed41e3aae7d9580ddde9c0fc1ed1b33158
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-07-15 00:04:48 +0200

    Bug fix in genome length for factory.

--- src/main/scala/fr/iscpif/mgo/algorithm/ga/NSGAIISigma.scala
+++ src/main/scala/fr/iscpif/mgo/algorithm/ga/NSGAIISigma.scala
@@ -7,7 +7,6 @@ package fr.iscpif.mgo.algorithm.ga
 
 import fr.iscpif.mgo.ga._
 import fr.iscpif.mgo.algorithm._
-import java.util.Random
 import fr.iscpif.mgo.crossover._
 import fr.iscpif.mgo.elitism._
 import fr.iscpif.mgo.mutation._
@@ -23,6 +22,6 @@ trait NSGAIISigma extends NSGAII
 
   type G = GAGenomeWithSigma  
       
-  val factory = GAGenomeWithSigma.factory(2)
+  val factory = GAGenomeWithSigma.factory(genomeSize)
   
 }
