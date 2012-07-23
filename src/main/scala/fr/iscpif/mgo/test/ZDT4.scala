6045b54af5176fe4250bd114ea4c92acf759b3b9
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-07-23 16:30:53 +0200

    Spacing.

--- src/main/scala/fr/iscpif/mgo/test/ZDT4.scala
+++ src/main/scala/fr/iscpif/mgo/test/ZDT4.scala
@@ -9,12 +9,12 @@ import fr.iscpif.mgo._
 import math._
 
 trait ZDT4 extends GAProblem {
+  def n: Int
   
-  def min = Vector.fill(10)(0.0)
-  def max = Vector(1.0 :: List.fill(9)(5.0) ::: Nil: _*)
+  def min = List.fill(10)(0.0)
+  def max = 1.0 :: List.fill(9)(5.0)
   
   def apply(x: IndexedSeq[Double]) = Vector(f1(x), f2(x))
-  def n: Int
   
   def f1(x: IndexedSeq[Double]) = x(0)
   def f2(x: IndexedSeq[Double]) = g(x) * (1 - sqrt(x(0) / g(x)))
