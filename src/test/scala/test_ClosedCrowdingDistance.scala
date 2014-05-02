01e48761d8a6ae63133d29d698858a64545fb988
Guillaume Chérel <guillaume.c.cherel@gmail.com>
2014-05-02 11:41:48 +0200

    added 1D crowding distance tests

--- src/test/scala/test_ClosedCrowdingDistance.scala
+++ src/test/scala/test_ClosedCrowdingDistance.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Guillaume Chérel 30/04/14
+ * Copyright (C) Guillaume Chérel 2/05/14
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -22,26 +22,27 @@ import org.scalacheck.Gen
 import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary.arbitrary
 
-import fr.iscpif.mgo.metric.CrowdingDistance
+import fr.iscpif.mgo.metric.ClosedCrowdingDistance
 
-object CrowdingDistanceSpecification extends Properties("CrowdingDistance") {
+object ClosedCrowdingDistanceSpecification extends Properties("ClosedCrowdingDistance") {
   property("1D 3 points not ordered") =
     forAll(Gen.choose(-1000.0, 1000.0), Gen.choose(0.0, 1000.0), Gen.choose(0.0, 1000.0)) {
       (x0: Double, distXMin: Double, distXMax: Double) =>
         val points = Vector(Vector(x0 - distXMin), Vector(x0 + distXMax), Vector(x0))
-        val cd = CrowdingDistance(points)
+        val cd = ClosedCrowdingDistance(points)
         val cdXMin = cd(0)()
         val cdXMax = cd(1)()
         val cdX0 = cd(2)()
-        all("xmin; test vector: " + points.toString |: Compare.doubles(cdXMin, Double.PositiveInfinity),
-          "xmax; test vector: " + points.toString |: Compare.doubles(cdXMax, Double.PositiveInfinity),
+        val maxMinusMin: Double = distXMin + distXMax
+        all("xmin; test vector: " + points.toString |: Compare.doubles(cdXMin, distXMin * 2 / maxMinusMin),
+          "xmax; test vector: " + points.toString |: Compare.doubles(cdXMax, distXMax * 2 / maxMinusMin),
           "x0; test vector: " + points.toString |: Compare.doubles(cdX0, 1.0))
     }
 
   property("1D 4 points ordered") =
     forAll(Gen.choose(-1000.0, 1000.0), Gen.choose(0.0, 1000.0), Gen.choose(0.0, 1000.0), Gen.choose(0.0, 1000.0)) {
       (x0: Double, dist01: Double, dist12: Double, dist23: Double) =>
-        val cd = CrowdingDistance(Vector(Vector(x0),
+        val cd = ClosedCrowdingDistance(Vector(Vector(x0),
           Vector(x0 + dist01),
           Vector(x0 + dist01 + dist12),
           Vector(x0 + dist01 + dist12 + dist23)))
@@ -49,5 +50,5 @@ object CrowdingDistanceSpecification extends Properties("CrowdingDistance") {
         all(Compare.doubles(cd(1)(), (dist01 + dist12) / maxMinusMin),
           Compare.doubles(cd(2)(), (dist12 + dist23) / maxMinusMin))
     }
-
 }
+
