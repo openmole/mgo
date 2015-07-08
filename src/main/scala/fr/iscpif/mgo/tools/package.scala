42250669cf7e12b23425caec5aa2be49da0f99fd
Guillaume Chérel <guillaume.c.cherel@gmail.com>
2015-07-08 14:53:19 +0200

    added time function for quick and dirty proviling

--- src/main/scala/fr/iscpif/mgo/tools/package.scala
+++ src/main/scala/fr/iscpif/mgo/tools/package.scala
@@ -106,4 +106,13 @@ package object tools {
         case None => a
       }
   }     */
+
+
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block // call-by-name
+    val t1 = System.nanoTime()
+    println(f"Elapsed time: ${(t1 - t0)}%,d ns")
+    result
+  }
 }
