0f69a2ccb4d09e504243d12b5d6b13414107f6b6
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-01-13 17:57:24 +0100

    Implement kind of breeding and elitism for rjmcmc.

--- src/main/scala/fr/iscpif/mgo/rjmcmc/sampler.scala
+++ src/main/scala/fr/iscpif/mgo/rjmcmc/sampler.scala
@@ -17,12 +17,15 @@
  */
 package fr.iscpif.mgo.rjmcmc
 
+import fr.iscpif.mgo.breeding.Breeding
 import fr.iscpif.mgo.contexts.RandomGen
+import fr.iscpif.mgo.elitism._
 import monocle.macros.Lenses
 
 import scala.util.Random
 import scalaz._
 import Scalaz._
+import fr.iscpif.mgo.tools._
 
 object sampler {
 
@@ -63,10 +66,64 @@ object sampler {
     for {
       rng <- implicitly[RandomGen[M]].random
     } yield {
-      val modif = elements.modify(_.map(_ + rng.nextGaussian()))
+      val modif = elements.modify(_.map(_ + rng.nextDouble() - 0.5))
       Transformed(c, modif, 1.0, 1.0, 1.0, 1.0, 1.0)
     }
   }
+
+  trait Evaluate[M[_]] {
+    def get: M[Vector[Int]]
+    def set(ids: Vector[Int]): M[Unit]
+    def clear: M[Unit]
+  }
+
+  def breeding[M[_]: Monad: Evaluate, EC, C](
+    get: (Vector[EC], Int) => EC,
+    configuration: EC => C) = Breeding[M, EC, C] { s =>
+    for {
+      eval <- implicitly[Evaluate[M]].get
+      _ <- implicitly[Evaluate[M]].clear
+    } yield eval.map { i => configuration(get(s, i)) }
+  }
+
+  def elitism[M[_]: Monad: RandomGen: Evaluate, EC, C](
+    id: EC => Int,
+    energy: EC => Double,
+    temperature: EC => Double,
+    age: monocle.Lens[EC, Long],
+    configuration: monocle.Lens[EC, C],
+    replace: (Vector[EC], EC) => Vector[EC]) = Elitism[M, EC] { ecs: Vector[EC] =>
+
+    val (newSolutions, oldSolutions) = ecs.partition(ec => age.get(ec) == 0)
+
+    for {
+      rng <- implicitly[RandomGen[M]].random
+      _ <- implicitly[Evaluate[M]].set(newSolutions.map(id))
+    } yield {
+      def recurse(solutions: Vector[EC], candidates: List[EC]): Vector[EC] = {
+        candidates match {
+          case Nil => solutions
+          case candidate :: tail =>
+            val challenger = solutions.random(rng)
+            val ei = energy(challenger)
+            val ej = energy(candidate)
+            val ti = temperature(challenger)
+            val tj = temperature(candidate)
+            val update = math.min(1.0, math.exp((ei - ej) * (1 / ti - 1 / tj)))
+
+            val updatedCandidate =
+              if (rng.nextDouble < update) configuration.set(configuration.get(challenger))(candidate)
+              else candidate
+
+            recurse(replace(solutions, updatedCandidate), tail)
+        }
+      }
+
+      recurse(oldSolutions, newSolutions.toList)
+    }
+
+  } andThen incrementGeneration[M, EC](age)
+
 }
 
 object TestRJMCMC extends App {
@@ -75,12 +132,14 @@ object TestRJMCMC extends App {
   import fr.iscpif.mgo.contexts.default._
 
   @Lenses case class Configuration(d: Vector[Double])
+  @Lenses case class Parameter(configuration: Configuration, temperature: Double, id: Int)
+  @Lenses case class EvaluatedConfiguration(parameter: Parameter, energy: Double)
 
-  val res =
-    unwrap(rjmcmcSampler[EvolutionState[Unit, ?], Configuration](
-      move[EvolutionState[Unit, ?], Configuration](Configuration.d)).
+  /*val res =
+    unwrap(rjmcmcSampler[EvolutionState[Unit, ?], Parameter](
+      move[EvolutionState[Unit, ?], Parameter](Parameter.configuration composeLens Configuration.d)).
       run(Configuration(Vector(0.0, 5.0))), EvolutionData[Unit](s = ()))
 
-  println(res)
+  println(res)*/
 }
 
