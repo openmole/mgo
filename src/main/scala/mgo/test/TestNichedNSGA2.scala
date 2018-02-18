d8a0a91a2ec5db272764b3015f0d878dfd9db4a5
Romain Reuillon <romain.reuillon@iscpif.fr>
2018-02-18 11:05:28 +0100

    Add an example for niched nsga2.

--- src/main/scala/mgo/test/TestNichedNSGA2.scala
+++ src/main/scala/mgo/test/TestNichedNSGA2.scala
@@ -1,49 +1,37 @@
-//package mgo.test
-//
-//import com.google.protobuf.wrappers.DoubleValue
-//import mgo._
-//import mgo.contexts._
-//import freedsl.dsl._
-//
-//object ZDT4NichedNSGAII extends App {
-//
-//  import algorithm.nichednsga2._
-//
-//  case class Phenotype(diversity: Double, optimisation: Double)
-//
-//  def computePhenotype(v: Vector[Double]) = {
-//    val values = zdt4.compute(v)
-//    Phenotype(values(0), values(1))
-//  }
-//
-//  def evolution[M[_]: Generation: Random: cats.Monad: StartTime: IO] = {
-//
-//    val nsga2 = NichedNSGA2(
-//      phenotype = computePhenotype,
-//      fitness = (p: Phenotype) => Vector(p.optimisation),
-//      niche = (p: Phenotype) => grid(Vector(0.1))(Vector(p.diversity)),
-//      mu = 10,
-//      lambda = 100,
-//      genomeSize = 6)
-//
-//    nsga2.
-//      until(afterGeneration(10000)).
-//      trace((s, is) => println(s.generation)).
-//      evolution
-//  }
-//
-//  val (finalState, finalPopulation) =
-//    NichedNSGA2.run(new util.Random(42)) { imp =>
-//      import imp._
-//      evolution[DSL].eval
-//    }
-//
-//  def disp = {
-//    val res = result(finalPopulation, zdt4.scale)
-//    res map { case (g, p, f) => g ++ Vector(p.diversity, p.optimisation) }
-//  }
-//
-//  println(disp.map(_.mkString(",")).mkString("\n"))
-//
-//}
-//
+package mgo.test
+
+import mgo._
+import mgo.contexts._
+import freedsl.dsl._
+
+object NichedNSGAII extends App {
+
+  import algorithm._
+
+  case class Phenotype(diversity: Double, optimisation: Double)
+
+  val nsga2 = Profile(
+    lambda = 100,
+    fitness = discreteSphere.compute,
+    continuous = discreteSphere.continuous(6),
+    discrete = discreteSphere.discrete(3),
+    niche = i => i.genome.discreteValues.take(2).toSeq)
+
+
+  def evolution[M[_]: Generation: Random: cats.Monad: StartTime: IO] = {
+    nsga2.
+      until(afterGeneration(100)).
+      trace((s, is) => println(s.generation)).
+      evolution
+  }
+
+  val (finalState, finalPopulation) =
+    Profile.run(new util.Random(42)) { imp =>
+      import imp._
+      evolution[DSL].eval
+    }
+
+  println(Profile.result(nsga2, finalPopulation).mkString("\n"))
+
+}
+
