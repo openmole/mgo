2699b94b434b24b3fca7d4309a2af5c3c77354f3
Juste Raimbault <juste.raimbault@polytechnique.edu>
2023-07-11 16:23:16 +0200

    OSE test

--- mgo/src/main/scala/mgo/test/TestOSE.scala
+++ mgo/src/main/scala/mgo/test/TestOSE.scala
@@ -26,10 +26,10 @@ object RastriginOSE extends App {
   val (finalState, finalPopulation) =
     ose.
       until(afterGeneration(5000)).
-      trace { (s, is) => println(s.generation + " " + s.s._1.size) }.
+      trace { (s, is) => println(s.generation + " " + s.s._1.length) }.
       eval(new util.Random(42))
 
-  File("/tmp/ose.csv") write OSE.result(ose, finalState, finalPopulation).map(_.continuous.mkString(",")).mkString("\n")
+  File("./test/ose.csv") write OSE.result(ose, finalState, finalPopulation).map(_.continuous.mkString(",")).mkString("\n")
 }
 
 object NoisyRastriginOSE extends App {
@@ -67,33 +67,40 @@ object NoisyRastriginOSE extends App {
  * Benchmark function used in
  *  Sambridge, M. (2001). Finding acceptable models in nonlinear inverse problems using a neighbourhood algorithm. Inverse Problems, 17(3), 387.
  *  in the paper: f0 = c*d*((e+g)*h + l) ; g not defined -> use f0 = c*d*(e*h + l)
+ *
+ *  Neighborhood algorithm (with confidence intervals in second paper)
+ *    Sambridge, M. (1999). Geophysical inversion with a neighbourhood algorithm—I. Searching a parameter space. Geophysical journal international, 138(2), 479-494.
+ *    Sambridge, M. (1999). Geophysical inversion with a neighbourhood algorithm—II. Appraising the ensemble. Geophysical Journal International, 138(3), 727-746.
+ *
  */
 object Sambridge2001OSE extends App {
   import algorithm._
   import niche._
   import OSE._
 
-  def dimensions = 5
+  def dimensions = 2
   def continuous(size: Int) = Vector.fill(size)(C(-2.0, 2.0))
 
   def f(x: Vector[Double]): Double = {
     val (x1, x2, x3, x4, x5) = (x(0), x(1), x(2), x(3), x(4))
-    val a = 5 / 2 * math.pow(x1 + 1 / 5, 2.0) + 5 / 4 * x2 * x2
-    val b = 5 * math.pow(x1 - 3 / 5, 2.0) + math.pow(x2 + 3 / 20, 2.0)
-    val c = 1 / 100 * (x1 * x1 + math.pow(x2 - 11 / 10, 2.0))
-    val d = math.pow(x1 - 1.0, 2.0) + 10 * math.pow(x2 - 1.0, 2.0)
+    val a = 2.5 * math.pow(x1 + 0.2, 2.0) + 1.25 * x2 * x2
+    val b = 5.0 * math.pow(x1 - 0.6, 2.0) + math.pow(x2 + 0.15, 2.0)
+    val c = 0.01 * (x1 * x1 + math.pow(x2 - 1.1, 2.0))
+    val d = math.pow(x1 - 1.0, 2.0) + 10.0 * math.pow(x2 - 1.0, 2.0)
     val e = 5 * (50 * math.pow(x2 - x1 * x1, 2.0) + math.pow(1 - x1, 2.0))
-    val h = math.pow(x1 + 7 / 10, 2.0)
+    val h = math.pow(x1 + 0.7, 2.0)
     val l = 5 * math.pow(x2 - 0.5, 2.0)
-    val f0 = c * d * (e * h + l)
-    a * b * math.log(1 + f0) + x3 * x3 + x4 * x4 + x5 * x5
+    val f0loc = c * d * (e * h + l)
+    a * b * math.log(1 + f0loc) + x3 * x3 + x4 * x4 + x5 * x5
   }
+
+  def f0(x: Vector[Double]): Double = f(Vector(x(0), x(1), 0.0, 0.0, 0.0))
 
   val ose = OSE(
     mu = 100,
     lambda = 100,
-    fitness = (x, _) => Vector(f(x)),
-    limit = Vector(0.1),
+    fitness = (x, _) => Vector(f0(x)),
+    limit = Vector(0.01),
     origin =
       (c, _) =>
         boundedGrid(
@@ -104,9 +111,9 @@ object Sambridge2001OSE extends App {
 
   val (finalState, finalPopulation) =
     ose.
-      until(afterGeneration(5000)).
+      until(afterGeneration(2000)).
       trace { (s, is) => println(s.generation + " " + s.s._1.length) }.
       eval(new util.Random(42))
 
-  File("./test/ose.csv") write OSE.result(ose, finalState, finalPopulation).map(_.continuous.mkString(",")).mkString("\n")
+  File("./test/ose.csv") write OSE.result(ose, finalState, finalPopulation).map(i => (i.continuous ++ Vector(f0(i.continuous))).mkString(",")).mkString("\n")
 }
