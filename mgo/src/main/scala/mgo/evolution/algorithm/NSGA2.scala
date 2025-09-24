1e99c7ceb335de4c193edc8c79c289802aea160d
Romain Reuillon <romain.reuillon@iscpif.fr>
2025-09-24 12:47:00 +0200

    Remove dead code

--- mgo/src/main/scala/mgo/evolution/algorithm/NSGA2.scala
+++ mgo/src/main/scala/mgo/evolution/algorithm/NSGA2.scala
@@ -108,26 +108,7 @@ case class NSGA2(
   operatorExploration: Double = 0.1,
   reject: Option[(IArray[Double], IArray[Int]) => Boolean] = None)
 
-object NSGA2Operations {
-
-  //  def breeding[M[_]: cats.Monad: Generation: Random, I, G](
-  //    fitness: I => Vector[Double],
-  //    genome: I => G,
-  //    genomeValues: G => Vector[Double],
-  //    buildGenome: (Vector[Double], Vector[Long]) => G)(crossover: GACrossover[M], mutation: GAMutation[M], lambda: Int): Breeding[M, I, G] = Breeding { population =>
-  //    for {
-  //      ranks <- paretoRankingMinAndCrowdingDiversity[M, I](fitness) apply population
-  //      breeding = applyOperators[M, I, Vector[Double]](crossover, mutation, tournament[M, I, (Lazy[Int], Lazy[Double])](ranks), genome andThen genomeValues) apply population
-  //      offspring <- breeding repeat ((lambda + 1) / 2)
-  //      offspringGenomes = offspring.flatMap {
-  //        case (o1, o2) =>
-  //          def gv1 = o1.map(math.clamp(_))
-  //          def gv2 = o2.map(math.clamp(_))
-  //          Vector(buildGenome(gv1), buildGenome(gv2))
-  //      }
-  //      sizedOffspringGenomes <- randomTake[M, G](offspringGenomes, lambda)
-  //    } yield sizedOffspringGenomes
-  //  }
+object NSGA2Operations:
 
   def adaptiveBreeding[S, I, G](
     fitness: I => Vector[Double],
@@ -186,13 +167,11 @@ object NSGA2Operations {
 //          (s, keepHighestRanked(cloneRemoved, ranks, mu))
 //      else (s, cloneRemoved)
 
-  def genomeDistance[I](
-    population: Vector[I],
-    values: I => (IArray[Double], IArray[Int])) =
+//  def genomeDistance[I](
+//    population: Vector[I],
+//    values: I => (IArray[Double], IArray[Int])) =
+//
+//    val crowding = CrowdingDistance.computeCrowdingDistance(population.map(i => values(i)._1.toVector))
+//    crowding
 
-    val crowding = CrowdingDistance.computeCrowdingDistance(population.map(i => values(i)._1.toVector))
-    crowding
 
-
-
-}
